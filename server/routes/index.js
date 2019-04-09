09c6364253a49b10d10e2017e8adccd79d5f78ba
Mark-A-A <markawai01@gmail.com>
2019-04-08 22:28:06 -0400

    default route for browser routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,20 +4,15 @@ const apiRoutes = require("./apiRoutes");
 
 // API Routes
 router.use("/api", apiRoutes);
-// API Routes
-// router.use("/auth", authRoutes);
 
-// HTTP status 404: NotFound
-router.use(function (req, res) {
-    res.status(404)        
-        .send('Not found');
-});
+// Auth Routes
+// router.use("/auth", authRoutes);
 
 // Send every request to the React app
 // // Define any API routes before this runs
-// app.get("*", function (req, res) {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+router.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 
 module.exports = router;
